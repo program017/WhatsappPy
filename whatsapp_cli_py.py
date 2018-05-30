977f183e6928fcc6022030d7d77fc4f8098efe5a
Rashid Aziz <rashidhbp@gmail.com>
2018-05-30 16:57:30 +0530

    Update whatsapp_cli_py.py

--- whatsapp_cli_py.py
+++ whatsapp_cli_py.py
@@ -6,6 +6,10 @@ def send(driver):
     name=raw_input('Enter the contact name or group name\n')
     message=raw_input('Enter message\n')
     try:
+        #search=driver.find_element_by_class_name('_2MSJr') # testing for search box
+        #msg_box.send_keys(name)
+        #search.click()
+        #print 'search complete'
         user=driver.find_element_by_xpath('//span[@title="{}"]'.format(str(name)))
         user.click()
     except:
@@ -46,38 +50,49 @@ def log(driver):
     fi=str(name)+'.txt'
     f=open(fi,'a')
     while True:
-        act=driver.find_element_by_xpath('.//span[@class="{}"]'.format('O90ur'))
-        if act.text=='click here for contact info':
-            pass
-        elif act.text.split(' ')[0]=='last':
-            temp=act.text.split(' ')
-            text=str(temp[2])+' '+str(temp[4])+' '+str(temp[5])+'\n'
-            f2=open(fi,'r')
-            flag=0
-            for lines in f2:
-                if str(lines.strip(' '))==str(text):
-                    flag=1
-            f2.close()
-            if flag==0:
-                f.write(text)
-            flag=0
-        if act.text=='online':
-            #print '{} is online'.format(name)
-            time=dtn.now().strftime("%Y-%m-%d %H:%M")
-            f2=open(fi,'r')
-            flag1=0
-            for lines in f2:
-                if str(lines.strip(' '))==str(time):
-                    flag1=1
-            if flag1==0:
-                f.write(text)
+        try:
+            act=driver.find_element_by_xpath('.//span[@class="{}"]'.format('O90ur'))
+            if act.text=='click here for contact info':
+                pass
+            elif act.text.split(' ')[0]=='last':
+                temp=act.text.split(' ')
+                text=str(temp[2])+' '+str(temp[4])+' '+str(temp[5])+'\n'
+                f2=open(fi,'r')
+                flag=0
+                for lines in f2:
+                    if str(lines.strip(' '))==str(text):
+                        flag=1
+                f2.close()
+                if flag==0:
+                    f.write(text)
+                flag=0
+            if act.text=='online':
+                #print '{} is online'.format(name)
+                time=dtn.now().strftime("%Y-%m-%d %H:%M")
+                f2=open(fi,'r')
+                flag1=0
+                for lines in f2:
+                    if str(lines.strip(' '))==str(time):
+                        flag1=1
+                if flag1==0:
+                    f.write(text)
+        except:
+            print '****************************'
+            print 'User have disabled last seen'
+            print '****************************'
+            print
         tm=tm-1
         t.sleep(1)
+        print 'Time left: {} secs'.format(tm)
         if tm<=0:
             f.close()
             return
 
 def activity(driver):
+    print '-----------------------------------------------'
+    print 'Warning this will only work for recent contacts'
+    print '-----------------------------------------------'
+    print
     inp=input('Enter your choice\n1.Send Message\n2.Check Messages\n3.Check online log activity\n4.Exit\n')
     if inp==1:
         send(driver)
